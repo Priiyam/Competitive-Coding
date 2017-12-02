11d6557460514d36db00f86f13c136ea1427ecb3
priiyam <crifte@gmail.com>
2017-12-02 09:59:10 +0530

    ADJLIST.py added

---
+++ CodeChef/ADJLIST.py
@@ -0,0 +1,47 @@
+class Vertex:
+    def __init__(self, n):
+        self.name = n
+        self.neighbors = list()
+
+    def add_neighbor(self, v):
+        if v not in self.neighbors:
+            self.neighbors.append(v)
+            self.neighbors.sort()
+
+
+class Graph:
+    vertices = {}
+
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+            self.vertices[vertex.name] = vertex
+            return True
+        else:
+            return False
+
+    def add_edge(self, u, v):
+        if u in self.vertices and v in self.vertices:
+            # my YouTube video shows a silly for loop here, but this is a much faster way to do it
+            self.vertices[u].add_neighbor(v)
+            self.vertices[v].add_neighbor(u)
+            return True
+        else:
+            return False
+
+    def print_graph(self):
+        for key in sorted(list(self.vertices.keys())):
+            print(key + str(self.vertices[key].neighbors))
+
+
+n, m = map(int, raw_input().split())
+g = Graph()
+for i in xrange(1, n+1):
+    g.add_vertex(Vertex(str(i)))
+edges = []
+for i in xrange(m):
+    edges.append(raw_input().replace(' ',''))
+for edge in edges:
+    g.add_edge(edge[:1], edge[1:])
+
+g.print_graph()
+
